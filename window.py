a8eaffc1ab2d4e5666b9ddb73b1270aa117bb6e7
Less is more <36774700+LessIsMorePy@users.noreply.github.com>
2018-04-21 00:18:12 -0500

    Update window.py

--- window.py
+++ window.py
@@ -7,7 +7,7 @@ a excel file.
 
 It should be mentioned that this application was designed for a specific task. 
 In this case, the main task is to receive a special format data from an antique
-printer that does not work well currently.
+printer that does not works well currently.
 
 The application has a logic to handle some issues of connexion serial port.
 
